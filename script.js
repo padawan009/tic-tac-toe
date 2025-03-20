23f27dff1cebad755b8bc59c3a29fef0fdc3ac34
Asya I <as.izmaylova@gmail.com>
2025-03-20 07:00:46 +0300

    added some func

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 let gameBoard = document.querySelector('.board');
 let blockCell = document.querySelectorAll('.board-cell');
-const restartBtn = document.querySelector('.restart-btn')
-const winnerDisplay = document.querySelector('.winner-text')
+const restartBtn = document.querySelector('.restart-btn');
+const winnerDisplay = document.querySelector('.winner-text');
+const body = document.querySelector('body');
 let marker = 'cross';
 let winner = '';
 let counter = 0;
+let winCounter1 = 0;
+let winCounter2 = 0;
 
 console.log(blockCell);
+
+blockCell.forEach((cell) => {
+    cell.addEventListener('click', () => {
+        if (winner || cell.classList.contains('circle') || cell.classList.contains('cross')) return;
+        cell.classList.add(marker);
+        cell.textContent = marker === 'cross' ? 'x' : 'o';
+        counter++;
+        if (checkWinner(marker)) return;
+        changeMarker();
+        noWinners();
+        
+    })
+})
 
 function changeMarker() {
     marker = marker === 'circle' ? 'cross' : 'circle';
 }
 
-changeMarker();
-
-blockCell.forEach((cell) => {
-    cell.addEventListener('click', () => {
-        if (winner || cell.classList.contains('circle') || cell.classList.contains('cross')) return;
-
-        cell.classList.add(marker);
-        cell.textContent = marker === 'cross' ? 'x' : 'o';
-        if (checkWinner(marker)) return;
-        counter++;
-        changeMarker();
-        noWinners();
-        console.log()
-    })
-})
 
 const Gameboard = (function() {
     let board = [
@@ -47,14 +48,6 @@ function Player (player, marker) {
 }
 
 function resetBoard() {
-    const board = Gameboard.getBoard();
-
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < 3; j++) {
-            board[i][j] = ''; 
-        }
-    }
-
     blockCell.forEach(cell => {
         cell.classList.remove('cross', 'circle', 'win-color');
         cell.textContent = '';
@@ -63,6 +56,9 @@ function resetBoard() {
     winner = ''; 
     gameBoard.style.pointerEvents = 'auto'; 
     counter = 0;
+    winnerDisplay.textContent = '';
+    marker = 'cross';
+
 }
 
 const winnerCombinations = [
@@ -72,23 +68,64 @@ const winnerCombinations = [
 ];
 
 function checkWinner(marker) {
-    let i = 0;
-    while (i < winnerCombinations.length) {
-        if (blockCell[winnerCombinations[i][0]].classList.contains(marker) && 
-        blockCell[winnerCombinations[i][1]].classList.contains(marker) && 
-        blockCell[winnerCombinations[i][2]].classList.contains(marker)) {
-            blockCell[winnerCombinations[i][0]].classList.add('win-color'); 
-            blockCell[winnerCombinations[i][1]].classList.add('win-color'); 
-            blockCell[winnerCombinations[i][2]].classList.add('win-color');
-            winner = marker === 'circle' ? 'Circle player win!' : 'Cross player win!';
-            // counter++;
+    for (let combination of winnerCombinations) {
+        if (
+            blockCell[combination[0]].classList.contains(marker) && 
+            blockCell[combination[1]].classList.contains(marker) && 
+            blockCell[combination[2]].classList.contains(marker)
+        ) {
+            combination.forEach(index => blockCell[index].classList.add('win-color'))
+            countWin(marker);
             endGame(winner);
             console.log(winner);
             return true;
         }
-        i++;
     }
     return false;
+}
+
+function countWin(marker) {
+    if (winCounter1 < 10 && winCounter2 < 10) {
+        if (marker === 'cross') {
+            winCounter1++;
+            winner = `Player 1 win!`;
+        
+        }
+        else {
+            winCounter2++;
+            winner = `Player 2 win!`;
+           
+        } 
+        displayWinCounter(winCounter1, winCounter2);
+    }
+    // if (winCounter1 === 10 || winCounter2 === 10) {
+    //     winner = winCounter1 === 10 ? 'Player 1' : 'Player 2';
+    //     endGame(`Victory belongs to Player 1!`);
+    // }
+        
+    }
+
+    // winner = marker === 'circle' ? 'Player 2 win!' : 'Player 1 win!';
+
+
+
+let winScore = document.createElement('p');
+winScore.classList.add('win-score');
+winScore.textContent = 'Player 1: 0 | Player 2: 0'
+body.appendChild(winScore);
+
+function displayWinCounter(win1, win2) {
+    if (winCounter1 === 10 || winCounter2 === 10) {
+        winner = winCounter1 === 10 ? 'Victory belongs to Player 1' : 'Victory belongs to Player 2'
+        winCounter1 = 0;
+        winCounter2 = 0;
+        winScore.textContent = 'Player 1: 0 | Player 2: 0';
+        endGame(winner);
+    }
+    else {
+        winScore.textContent = `Player 1: ${win1} | Player 2: ${win2}`;
+    }
+
 }
 
 function noWinners() {
@@ -105,5 +142,4 @@ function endGame(winner) {
 
 restartBtn.addEventListener('click', () => {
     resetBoard();
-    winnerDisplay.textContent = '';
 })
