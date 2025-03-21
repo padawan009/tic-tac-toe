1393d98f9b7ccc1b0094da6566d3af0be8735fa9
Asya I <as.izmaylova@gmail.com>
2025-03-21 13:04:24 +0300

    added changes

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const winnerDisplay = document.querySelector('.winner-text');
 const winScore = document.querySelector('.win-score');
 const inputValuePlayer1 = document.querySelector('#player1');
 const inputValuePlayer2 = document.querySelector('#player2');
-const submitInputBtn = document.querySelectorAll('.submit-name-btn')
+const submitInputBtn = document.querySelectorAll('.submit-name-btn');
+
 let marker = 'cross';
-let winner = '';
+let winnerText = '';
 let cellCounter = 0;
 let winCounter1 = 0;
 let winCounter2 = 0;
@@ -17,51 +18,40 @@ let player2 = '';
 console.log(blockCell);
 
 function checkInputs() {
-    if (!inputValuePlayer1.value.trim() && !inputValuePlayer2.value.trim()) {
+    if (!inputValuePlayer1.value.trim() || !inputValuePlayer2.value.trim()) {
         alert('Enter the names of the players before starting the game.!');
-        return false;
-    }
-    else if (!inputValuePlayer1.value.trim() || !inputValuePlayer2.value.trim()) {
-        alert('Enter the names of BOTH players!');
         return false;
     }
     return true;
 }
 
-function submitInputs() {
-    submitInputBtn.forEach(button => {
-        button.addEventListener('click', (event) => {
-            event.preventDefault();
-            player1 = inputValuePlayer1.value.trim() || 'Player 1';
-            player2 = inputValuePlayer2.value.trim() || 'Player 2';
 
-            winCounter1 = 0;
-            winCounter2 = 0;
+document.querySelector('.inputs-block').addEventListener('submit', (event) => {
+    event.preventDefault();
+    player1 = inputValuePlayer1.value.trim() || 'Player 1';
+    player2 = inputValuePlayer2.value.trim() || 'Player 2';
 
-            winScore.innerHTML = `${player1}: ${winCounter1} | ${player2}: ${winCounter2}`;
-            checkInputs();
-        })
-    });
-}
+    winCounter1 = 0;
+    winCounter2 = 0;
+
+    winScore.innerHTML = `${player1}: ${winCounter1} | ${player2}: ${winCounter2}`;
+});
 
 
-function startGame() {
-    blockCell.forEach((cell) => {
-        cell.addEventListener('click', () => {
-            if (!checkInputs()) return;
+gameBoard.addEventListener('click', (event) => {
+    const cell = event.target;
+    if (!cell.classList.contains('board-cell')) return; 
 
-            if (winner || cell.classList.contains('circle') || cell.classList.contains('cross')) return;
+    if (!checkInputs() || winnerText || cell.classList.contains('circle') || cell.classList.contains('cross')) return;
 
-            cell.classList.add(marker);
-            cell.textContent = marker === 'cross' ? 'x' : 'o';
-            cellCounter++;
+    cell.classList.add(marker);
+    cell.textContent = marker === 'cross' ? 'x' : 'o';
+    cellCounter++;
 
-            if (checkWinner(marker)) return;
-            changeMarker();
-            noWinners();
-        })
-    })
-}
+    if (checkWinner(marker)) return;
+    changeMarker();
+    noWinners();
+});
 
 
 function changeMarker() {
@@ -88,7 +78,7 @@ function resetBoard() {
         cell.textContent = '';
     });
 
-    winner = ''; 
+    winnerText = ''; 
     gameBoard.style.pointerEvents = 'auto'; 
     cellCounter = 0;
     winnerDisplay.textContent = '';
@@ -98,7 +88,7 @@ function resetBoard() {
         winCounter1 = 0;
         winCounter2 = 0;
         winScore.innerHTML = `${player1}: 0 | ${player2}: 0`;
-    }
+    }  
 }
 
 const winnerCombinations = [
@@ -114,10 +104,10 @@ function checkWinner(marker) {
             blockCell[combination[1]].classList.contains(marker) && 
             blockCell[combination[2]].classList.contains(marker)
         ) {
-            combination.forEach(index => blockCell[index].classList.add('win-color'))
+            combination.forEach(index => blockCell[index].classList.add('win-color'));
             countWin(marker);
-            endGame(winner);
-            console.log(winner);
+            endGame(winnerText);
+            console.log(winnerText);
             return true;
         }
     }
@@ -128,39 +118,36 @@ function countWin(marker) {
     if (winCounter1 < 10 && winCounter2 < 10) {
         if (marker === 'cross') {
             winCounter1++;
-            winner = `${player1} wins!`;
+            winnerText = `${player1} wins!`;
         }
         else {
             winCounter2++;
-            winner = `${player2} wins!`; 
+            winnerText = `${player2} wins!`; 
         } 
-        displayWinCounter(winCounter1, winCounter2);
+        displayWinScore(winCounter1, winCounter2);
     }
 }
 
-function displayWinCounter(winCounter1, winCounter2) {
+function displayWinScore(winCounter1, winCounter2) {
     if (winCounter1 === 10 || winCounter2 === 10) {
-        winner = winCounter1 === 10 ? `Victory belongs to ${player1}` : `Victory belongs to ${player2}`
-        endGame(winner);
+        winnerText = winCounter1 === 10 ? `Victory belongs to ${player1}!` : `Victory belongs to ${player2}!`
+        endGame(winnerText);
     }
     winScore.innerHTML = `${player1}: <span>${winCounter1}</span> | ${player2}: <span>${winCounter2}</span>`;
 }
 
 function noWinners() {
-    if (!winner && cellCounter === 9) {
-        winner = 'What a pity, there are no winners!'
-        endGame(winner);
+    if (!winnerText && cellCounter === 9) {
+        winnerText = 'What a pity, there are no winners!'
+        endGame(winnerText);
     }
 }
 
-function endGame(message) {
+function endGame(winnerText) {
     gameBoard.style.pointerEvents = 'none';
-    winnerDisplay.textContent = message;
+    winnerDisplay.textContent = winnerText;
 }
 
 restartBtn.addEventListener('click', () => {
     resetBoard();
 })
-
-submitInputs();
-startGame();
