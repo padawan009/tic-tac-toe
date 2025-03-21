a3deae262a7634ab9e2875c826005b67f04e77b4
Asya I <as.izmaylova@gmail.com>
2025-03-21 06:44:54 +0300

    final

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let gameBoard = document.querySelector('.board');
 let blockCell = document.querySelectorAll('.board-cell');
 const restartBtn = document.querySelector('.restart-btn');
 const winnerDisplay = document.querySelector('.winner-text');
-const main = document.querySelector('main');
+const winScore = document.querySelector('.win-score');
 const inputValuePlayer1 = document.querySelector('#player1');
 const inputValuePlayer2 = document.querySelector('#player2');
 const submitInputBtn = document.querySelectorAll('.submit-name-btn')
 let marker = 'cross';
 let winner = '';
-let counter = 0;
+let cellCounter = 0;
 let winCounter1 = 0;
 let winCounter2 = 0;
 let player1 = '';
@@ -16,47 +16,71 @@ let player2 = '';
 
 console.log(blockCell);
 
-submitInputBtn.forEach(button => {
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-    })
-})
+function checkInputs() {
+    if (!inputValuePlayer1.value.trim() && !inputValuePlayer2.value.trim()) {
+        alert('Enter the names of the players before starting the game.!');
+        return false;
+    }
+    else if (!inputValuePlayer1.value.trim() || !inputValuePlayer2.value.trim()) {
+        alert('Enter the names of BOTH players!');
+        return false;
+    }
+    return true;
+}
 
-blockCell.forEach((cell) => {
-    cell.addEventListener('click', () => {
-        if (winner || cell.classList.contains('circle') || cell.classList.contains('cross')) return;
-        cell.classList.add(marker);
-        cell.textContent = marker === 'cross' ? 'x' : 'o';
-        counter++;
-        if (checkWinner(marker)) return;
-        changeMarker();
-        noWinners();
-        
+function submitInputs() {
+    submitInputBtn.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            player1 = inputValuePlayer1.value.trim() || 'Player 1';
+            player2 = inputValuePlayer2.value.trim() || 'Player 2';
+
+            winCounter1 = 0;
+            winCounter2 = 0;
+
+            winScore.innerHTML = `${player1}: ${winCounter1} | ${player2}: ${winCounter2}`;
+            checkInputs();
+        })
+    });
+}
+
+
+function startGame() {
+    blockCell.forEach((cell) => {
+        cell.addEventListener('click', () => {
+            if (!checkInputs()) return;
+
+            if (winner || cell.classList.contains('circle') || cell.classList.contains('cross')) return;
+
+            cell.classList.add(marker);
+            cell.textContent = marker === 'cross' ? 'x' : 'o';
+            cellCounter++;
+
+            if (checkWinner(marker)) return;
+            changeMarker();
+            noWinners();
+        })
     })
-})
+}
+
 
 function changeMarker() {
     marker = marker === 'circle' ? 'cross' : 'circle';
 }
 
 
-const Gameboard = (function() {
-    let board = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', ''],
-    ];
+// const Gameboard = (function() {
+//     let board = [
+//         ['', '', ''],
+//         ['', '', ''],
+//         ['', '', ''],
+//     ];
 
-    function getBoard() {
-        return board;
-    }
-    return { getBoard };
-})();
-
-function Player (player, marker) {
-    this.player = player;
-    this.marker = marker;
-}
+//     function getBoard() {
+//         return board;
+//     }
+//     return { getBoard };
+// })();
 
 function resetBoard() {
     blockCell.forEach(cell => {
@@ -66,10 +90,15 @@ function resetBoard() {
 
     winner = ''; 
     gameBoard.style.pointerEvents = 'auto'; 
-    counter = 0;
+    cellCounter = 0;
     winnerDisplay.textContent = '';
     marker = 'cross';
 
+    if (winCounter1 === 10 || winCounter2 === 10) {
+        winCounter1 = 0;
+        winCounter2 = 0;
+        winScore.innerHTML = `${player1}: 0 | ${player2}: 0`;
+    }
 }
 
 const winnerCombinations = [
@@ -99,59 +128,39 @@ function countWin(marker) {
     if (winCounter1 < 10 && winCounter2 < 10) {
         if (marker === 'cross') {
             winCounter1++;
-            winner = `Player 1 win!`;
-        
+            winner = `${player1} win!`;
         }
         else {
             winCounter2++;
-            winner = `Player 2 win!`;
-           
+            winner = `${player2} win!`; 
         } 
         displayWinCounter(winCounter1, winCounter2);
     }
-    // if (winCounter1 === 10 || winCounter2 === 10) {
-    //     winner = winCounter1 === 10 ? 'Player 1' : 'Player 2';
-    //     endGame(`Victory belongs to Player 1!`);
-    // }
-        
-    }
+}
 
-    // winner = marker === 'circle' ? 'Player 2 win!' : 'Player 1 win!';
-
-
-
-let winScore = document.createElement('p');
-winScore.classList.add('win-score');
-winScore.innerHTML = 'Player 1: 0 | Player 2: 0';
-main.appendChild(winScore);
-
-function displayWinCounter(win1, win2) {
+function displayWinCounter(winCounter1, winCounter2) {
     if (winCounter1 === 10 || winCounter2 === 10) {
-        winner = winCounter1 === 10 ? 'Victory belongs to Player 1' : 'Victory belongs to Player 2'
-        winCounter1 = 0;
-        winCounter2 = 0;
-        winScore.innerHTML = 'Player 1: 0 | Player 2: 0';
+        winner = winCounter1 === 10 ? `Victory belongs to ${player1}` : `Victory belongs to ${player2}`
         endGame(winner);
     }
-    else {
-        // winScore.textContent = `Player 1: ${win1} | Player 2: ${win2}`;
-        winScore.innerHTML = `Player 1: <span>${win1}</span> | Player 2: <span>${win2}</span>`;
-    }
-
+    winScore.innerHTML = `${player1}: <span>${winCounter1}</span> | ${player2}: <span>${winCounter2}</span>`;
 }
 
 function noWinners() {
-    if (!winner && counter === 9) {
+    if (!winner && cellCounter === 9) {
         winner = 'What a pity, there are no winners!'
         endGame(winner);
     }
 }
 
-function endGame(winner) {
+function endGame(message) {
     gameBoard.style.pointerEvents = 'none';
-    winnerDisplay.textContent = winner;
+    winnerDisplay.textContent = message;
 }
 
 restartBtn.addEventListener('click', () => {
     resetBoard();
 })
+
+submitInputs();
+startGame();
